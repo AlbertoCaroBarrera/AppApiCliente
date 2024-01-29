5d0330fa74bd9d35752c770933181a5e9a112b04
alberto <albertocarobarrera@gmail.com>
2024-01-29 21:30:24 +0100

    añadiendo consultas

--- hotel/urls.py
+++ hotel/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('habitaciones', views.habitaciones_lista_api, name='habitaciones_lista_api'),
     path('reservas-mejorado', views.reservas_lista_api_mejorada, name='reservas_lista_api_mejorada'),
     path('clientes-mejorado', views.clientes_lista_api_mejorada, name='clientes_lista_api_mejorada'),
-    path('usuarios-mejorado', views.usuarios_lista_api_mejorada, name='usuarios_lista_api_mejorada'),
     path('habitaciones-mejorado', views.habitaciones_lista_api_mejorada, name='habitaciones_lista_api_mejorada'),
 
 ]
