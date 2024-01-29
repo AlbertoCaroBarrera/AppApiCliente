d98136c4245d4b90df64bb523a81c0b8dec7c998
alberto <albertocarobarrera@gmail.com>
2024-01-29 21:40:30 +0100

    añadiendo urls

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
+    path('',views.index,name='index'),
     path('clientes', views.clientes_lista_api, name='clientes_lista_api'),
     path('reservas', views.reservas_lista_api, name='reservas_lista_api'),
     path('usuarios', views.usuarios_lista_api, name='usuarios_lista_api'),
@@ -8,6 +9,8 @@ urlpatterns = [
     path('reservas-mejorado', views.reservas_lista_api_mejorada, name='reservas_lista_api_mejorada'),
     path('clientes-mejorado', views.clientes_lista_api_mejorada, name='clientes_lista_api_mejorada'),
     path('habitaciones-mejorado', views.habitaciones_lista_api_mejorada, name='habitaciones_lista_api_mejorada'),
-
+    path('clientes_lista_api', views.clientes_lista_api, name='clientes_lista_api'),
+    path('reservas_lista_api', views.reservas_lista_api, name='reservas_lista_api'),
+    path('cliente_busqueda_simple', views.cliente_busqueda_simple, name='cliente_busqueda_simple'),
+    path('cliente_busqueda_avanzada', views.cliente_busqueda_avanzada, name='cliente_busqueda_avanzada'),
 ]
-
